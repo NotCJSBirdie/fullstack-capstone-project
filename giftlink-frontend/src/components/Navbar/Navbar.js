4ebfdd02222b8591c89250148c689ce5163b6bb7
“NotCJSBirdie” <serquinajhunlo@gmail.com>
2025-09-12 03:46:19 -0400

    deployment

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAppContext } from '../../context/AuthContext';
 
 export default function Navbar() {
+    const { isLoggedIn, setIsLoggedIn } = useAppContext();
+    const username = sessionStorage.getItem('name');
+
+    const handleLogout = () => {
+        sessionStorage.clear();
+        setIsLoggedIn(false);
+        window.location.href = "/app/login";
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">GiftLink</a>
+            <Link className="navbar-brand" to="/">GiftLink</Link>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-
                     <li className="nav-item">
-                    <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
+                        <Link className="nav-link" to="/app">Gifts</Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/app/search">Search</Link>
                     </li>
+                    {!isLoggedIn ? (
+                        <>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/app/login">Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/app/register">Register</Link>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li className="nav-item">
+                                <span className="navbar-text mx-2"><b>{username}</b></span>
+                            </li>
+                            <li className="nav-item">
+                                <button className="btn btn-outline-danger" onClick={handleLogout}>Logout</button>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </div>
         </nav>
